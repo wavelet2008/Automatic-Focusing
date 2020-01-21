3ddefd7f4f86180c51f7544f0323401421878199
weihuajing <jerryweihuajing@126.com>
2020-01-21 22:04:51 +0800

    relocate

--- C++/Source/calculation_scene_discrimination.cpp
+++
@@ -1,8 +0,0 @@
-// -*- coding: utf-8 -*-
-/******************************************************************************
-@author: Wei Huajing
-@company: KAMERAWERK
-@e-mail: jerryweihuajing@126.com
-
-@title: Module-Discrimination of foreground and background
-******************************************************************************/
