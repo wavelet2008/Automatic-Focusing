cc476fdc205d572173d6beb9a96577fed5e1e866
jerryweihuajing <jerryweihuajing@126.com>
2020-07-15 18:24:48 +0800

    gorgeous?

--- configuration_auto_focus_calibration.py
+++ configuration_auto_focus_calibration.py
@@ -18,7 +18,7 @@ offset_end=5
 n_interval=23
 
 #regard the distance grow meter by 1 millimeter
-interval_offset=0.1
+interval_offset=0.0666
 
 offset_start=offset_end-interval_offset*n_interval
 
@@ -26,7 +26,7 @@ canvas=np.zeros((1080,1920))
 
 '''add the circle lap by lap'''
 radius_circle_original=13
-interval_circle_original=66
+interval_circle_original=47
 
 #generate output folder path
 equal_folder_path=os.getcwd()+'//calibration plane frames/equal'
