f51eb73ca15922e429c654511a63d2d36a1aaecc
jerryweihuajing <jerryweihuajing@126.com>
2020-07-17 17:58:56 +0800

    GLCM launch

--- script_simulation.py
+++ script_simulation.py
@@ -21,43 +21,59 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import MultipleLocator
 from matplotlib.font_manager import FontProperties
 
-'''VCM'''
-imgs_folder=r'C:\Users\Administrator\Desktop\Ink_Step50'
-output_folder=r'C:\Users\Administrator\Desktop\Contrast\Parameter'
+# '''VCM'''
+# imgs_folder=r'C:\Users\Administrator\Desktop\Ink_Step50'
+# output_folder=r'C:\Users\Administrator\Desktop\Contrast\Parameter'
 
-#E_P.Experiment5AreaFactor(imgs_folder,output_folder)
-#E_P.Experiment5AreaWeight(imgs_folder,output_folder)
-#E_P.ExperimentBlockModuleRatio(imgs_folder,output_folder)
+# #E_P.Experiment5AreaFactor(imgs_folder,output_folder)
+# #E_P.Experiment5AreaWeight(imgs_folder,output_folder)
+# #E_P.ExperimentBlockModuleRatio(imgs_folder,output_folder)
 
-#img_gray=O_I.BatchImages(imgs_folder)[1][10]
-#plt.imshow(img_gray,cmap='gray')
-#cv2.imshow('gray',img_gray)
+# #img_gray=O_I.BatchImages(imgs_folder)[1][10]
+# #plt.imshow(img_gray,cmap='gray')
+# #cv2.imshow('gray',img_gray)
 
-'''tmp-Luminance, Near-others'''
-experiment_folder=r'C:\Users\Administrator\Desktop\Experiment\Luminance'
+# '''tmp-Luminance, Near-others'''
+# experiment_folder=r'C:\Users\Administrator\Desktop\Experiment\Luminance'
  
-#E_P.ExperimentContrastComparison(experiment_folder,'Constant')
-#E_P.ExperimentContrastComparison(experiment_folder,'Advanced')
-#E_P.ExperimentContrastComparison(experiment_folder,'Standard Deviation')
+# #E_P.ExperimentContrastComparison(experiment_folder,'Constant')
+# #E_P.ExperimentContrastComparison(experiment_folder,'Advanced')
+# #E_P.ExperimentContrastComparison(experiment_folder,'Standard Deviation')
     
-'''[:2]-tmp, [2:]-Near'''
-root_folder=r'C:\Users\Administrator\Desktop\Experiment'
+# '''[:2]-tmp, [2:]-Near'''
+# root_folder=r'C:\Users\Administrator\Desktop\Experiment'
 
-list_imgs_folder_name=[r'Luminance\Low',
-                       r'Luminance\High',
-                       r'Texture\Unobvious',
-                       r'Texture\Obvious',
-                       r'Distance\Invarious',
-                       r'Distance\Various']
+# list_imgs_folder_name=[r'Luminance\Low',
+#                        r'Luminance\High',
+#                        r'Texture\Unobvious',
+#                        r'Texture\Obvious',
+#                        r'Distance\Invarious',
+#                        r'Distance\Various']
 
-list_imgs_folder=[root_folder+'\\'+this_imgs_folder_name for this_imgs_folder_name in list_imgs_folder_name[:2]]
+# list_imgs_folder=[root_folder+'\\'+this_imgs_folder_name for this_imgs_folder_name in list_imgs_folder_name[:2]]
 
-#E_P.ExperimentOverall(list_imgs_folder)
+# #E_P.ExperimentOverall(list_imgs_folder)
 
-'''optimized frames construction'''
+# '''optimized frames construction'''
 
-imgs_folder=r'C:\Users\Administrator\Desktop\Experiment\Luminance\Low'
+# imgs_folder=r'C:\Users\Administrator\Desktop\Experiment\Luminance\Low'
 
-C_P_S.FullSweep(imgs_folder,'CMAN')
+# # C_P_S.FullSweep(imgs_folder,'CMAN')
 
 '''gif look like peak search'''
+
+img = cv2.imread("test.jpg")
+    
+try:
+    
+    img_shape=img.shape
+    
+except:
+    
+    print('imread error')
+    
+img=cv2.resize(img,(int(img_shape[1]/20),int(img_shape[0]/20)),interpolation=cv2.INTER_CUBIC)
+
+img_gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+contrast=C_T_F.MapTextureFeature(img_gray,0)['Contrast']
