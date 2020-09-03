828f4f03c666dce03d2da1525c5a8858da450d50
jerryweihuajing <jerryweihuajing@126.com>
2020-09-03 09:46:02 +0800

    Update script_simulation.py

--- python/script_simulation.py
+++ python/script_simulation.py
@@ -19,9 +19,9 @@ from __init__ import *
 
 # imgs_folder=r'C:\Users\ASUS\Desktop\Experiment\poLight-Medium-Coarse\60mm'
 # imgs_folder=r'C:\Users\ASUS\Desktop\Experiment\poLight-Medium-Fine\60mm'
-# imgs_folder=r'C:\Users\ASUS\Desktop\Experiment\Bottle\20cm-dark'
+imgs_folder=r'C:\Users\魏华敬\Desktop\Experiment\Bottle\20cm-dark'
 
-# C_P_S.FullSweep(imgs_folder,'Boccignone','5-Area')
+C_P_S.FullSweep(imgs_folder,'Boccignone','5-Area')
 
 # contain coarse and fine
 # S_A_F.AutoFocusAnimation(imgs_folder,'Boccignone','Center')
@@ -37,8 +37,8 @@ from __init__ import *
     #contain coarse and fine
     # S_A_F.ImageAndContrast(this_imgs_folder,'Boccignone','Center')
 
-l=[1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1]
-idx_peak=C_P_S.GlobalSearch(l)
+# l=[1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1]
+# idx_peak=C_P_S.GlobalSearch(l)
 
 '''JL method'''
 
